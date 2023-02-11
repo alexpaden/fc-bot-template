71de8a64d640c579caab1e06dcfe1d1537519b48
Alex Paden <padenfool@gmail.com>
2023-02-11 17:53:27 -0600

    Update command_2 & format

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
-from farcaster import MerkleApiClient
 from dotenv import load_dotenv
+from farcaster import MerkleApiClient
+from farcaster.models import Parent
+
 
 
 if __name__ == "__main__":
@@ -9,7 +11,7 @@ if __name__ == "__main__":
     print(fcc.get_healthcheck())
 
     def handle_command(notif):
-    # Determine the command to be performed based on the event data
+        # Determine the command to be performed based on the event data
         if notif.content.cast.text.startswith(os.getenv("USERNAME") + " command_1"):
             try:
                 # Perform command 1
@@ -35,27 +37,33 @@ if __name__ == "__main__":
         # Log the error and take any necessary action to handle it
         print(f"An error occurred: {error}")
 
-    def perform_command_1(data):
+    def perform_command_1(notif):
         print("Running Command 1!")
-        response = fcc.get_cast("0xcd5082e1110a1ead6acb57bd8ed2e209cef6f892db64be02c3f8cc42247648b7")
+        response = fcc.get_cast(
+            hash="0xcd5082e1110a1ead6acb57bd8ed2e209cef6f892db64be02c3f8cc42247648b7"
+        )
         print(response.cast.hash)
         print(response.cast.author.username)
         print(response.cast.text)
 
-    def perform_command_2(data):
-        print("Running Command 1!")
-        response = fcc.post_cast(text="Hello World! This is a test cast for bot template.")
+    def perform_command_2(notif):
+        print("Running Command 2!")
+        response = fcc.post_cast(
+            "Hello World",
+            parent=Parent(
+                fid=notif.content.cast.author.fid, hash=notif.content.cast.hash
+            ),
+        )
         print(response.cast.hash)
         print(response.cast.text)
-        
-    def perform_command_3(data):
+
+    def perform_command_3(notif):
         print("Running Command 3!")
         # This command will cause an error to be thrown
-        fcc.get_cast("1234567890")
+        fcc.get_cast(hash="1234567890")
         print("This line will not be printed")
-        
+
     for notif in fcc.stream_notifications(max_counter=120):
         if notif and notif.content.cast.text.startswith(os.getenv("USERNAME")):
             handle_command(notif)
             pass
-        
