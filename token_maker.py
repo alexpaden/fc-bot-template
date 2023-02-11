4b16b2461169c6b6845046940b6a14c26f854bce
Alex Paden <padenfool@gmail.com>
2023-02-11 17:35:04 -0600

    Create token_maker.py
    generates new access tokens

---
+++ token_maker.py
@@ -0,0 +1,23 @@
+import os
+from farcaster import MerkleApiClient
+from dotenv import load_dotenv
+
+# THIS FILE GENERATES AN ACCESS TOKEN USING YOUR MNEMONIC PHRASE FROM .env
+# RUN VIA 'python3 ./token_maker.py' AND COPY THE OUTPUT TO YOUR .env FILE
+
+load_dotenv()
+
+# Make sure to set FARC_MNEMONIC in your .env file
+fcc = MerkleApiClient(os.getenv("FARC_MNEMONIC"))
+
+# prints true if the client is connected to the farcaster node
+print(fcc.get_healthcheck())
+
+# expires in 1 year
+minutes = 525600 
+
+# Generate a new access token
+access_token = fcc.create_new_auth_token(expires_in=minutes)
+
+# save it to FARC_SECRET in your .env file
+print(access_token)
